400658640fd5fbb0e59b824474e62dfa4693ab04
carlstep <carlstep333@me.com>
2023-05-14 11:22:38 +0700

    quiz.dart and questions_screen.dart

---
+++ lib/quiz.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import 'package:max_quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 1, 84, 4),
+                Colors.green,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    );
+  }
+}
