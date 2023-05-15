2f6e45e938aee0fd6eee8a75c92341f4d6923446
carlstep <carlstep333@me.com>
2023-05-15 08:07:29 +0700

    questions_screen format and data

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -10,6 +10,29 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('QuestionsScreen');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Question Text...'),
+          const SizedBox(
+            height: 40,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 1'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 1'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 1'),
+          ),
+        ],
+      ),
+    );
   }
 }
